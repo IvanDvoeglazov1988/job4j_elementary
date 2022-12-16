27769c82195b2af89b85d964857726c2161dfc51
IvanDvoeglazov1988 <id@stk-online.ru>
2022-12-16 19:48:22 +0500

    4.3.1. Сокращенный оператор && [#333783]

---
+++ src/main/java/ru/job4j/condition/Gamess.java
@@ -0,0 +1,18 @@
+package ru.job4j.condition;
+
+public class Gamess {
+    public static void permission(boolean allowByParent, boolean hasMoney) {
+        if (allowByParent & hasMoney) {
+            System.out.println("I can go to the computer club.");
+        } else {
+            System.out.println("I can't.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Gamess.permission(true, true);
+        Gamess.permission(true, false);
+        Gamess.permission(false, true);
+        Gamess.permission(false, false);
+    }
+}
