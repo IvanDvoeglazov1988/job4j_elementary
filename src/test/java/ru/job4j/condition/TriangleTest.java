7f50935cbe334bd74896611b6bdaa78cfe06682e
IvanDvoeglazov1988 <id@stk-online.ru>
2022-12-18 09:26:52 +0500

    4.3.5. Множественное логическое выражение И [#9461 #372023]

--- src/test/java/ru/job4j/condition/TriangleTest.java
+++ src/test/java/ru/job4j/condition/TriangleTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TriangleTest {
-
     @Test
     public void whenExist() {
         double ab = 2.0;
