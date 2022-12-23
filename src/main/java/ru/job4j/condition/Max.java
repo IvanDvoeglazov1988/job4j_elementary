ee4a249a408ec0cd33fc54368910ad326e63cfee
IvanDvoeglazov1988 <id@stk-online.ru>
2022-12-23 21:34:23 +0500

    3. Перегрузить метод max для трех чисел [#122644]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -4,4 +4,29 @@ public class Max {
     public static int max(int left, int right) {
         return left > right ? left : right;
     }
+
+    public static int max(int first, int second, int three) {
+        int max = first;
+        if (second > max) {
+            max = second;
+        }
+        if (three > max) {
+            max = three;
+        }
+        return max;
+    }
+
+    public static int max(int first, int second, int three, int four) {
+        int max = first;
+        if (second > max) {
+            max = second;
+        }
+        if (three > max) {
+            max = three;
+        }
+        if (four > max) {
+            max = four;
+        }
+        return max;
+    }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -23,11 +23,23 @@ public class MaxTest {
     }
 
     @Test
-    public void whenMax10To10Then10() {
-        int left = 10;
-        int right = 10;
-        int result = Max.max(left, right);
-        int expected = 10;
+    public void whenMax10To15To20Then20() {
+        int one = 10;
+        int two = 15;
+        int three = 20;
+        int result = Max.max(one, two, three);
+        int expected = 20;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenMax10To15To20To40Then40() {
+        int one = 10;
+        int two = 15;
+        int three = 20;
+        int four = 40;
+        int result = Max.max(one, two, three, four);
+        int expected = 40;
         assertThat(result).isEqualTo(expected);
     }
 }
